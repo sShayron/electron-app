0829f414373eed0660a07a9580cf11813f9fdfb4
Shayron Aguiar <shayron.aguair@gmail.com>
2016-07-12 21:46:15 -0300

    iniciando

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const electron = require('electron');
+const app = electron.app;﻿
+
+app.on('ready', function(){
+  var mainWindow = new electron.BrowserWindow({
+    width: 800,
+    height: 600
+  })
+})
